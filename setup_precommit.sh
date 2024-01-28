10510086a6a5d3f56a0d3b99b7117b4282cf40c9
Dmitry <pro100pro10010@gmail.com>
2024-01-28 17:59:24 +0300

    Fix [setup_precommit.sh] global

--- setup_precommit.sh
+++ setup_precommit.sh
@@ -2,7 +2,7 @@
 echo "#!/bin/sh
 ruff format .
 ruff check . --fix --exit-non-zero-on-fix
-# FILES=\$(git diff --diff-filter=d --name-only)
-# git add \$FILES requirements.txt
+FILES=\$(git diff --diff-filter=d --name-only)
+git add \$FILES
 " > .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
