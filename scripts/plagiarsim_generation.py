be2dae53f4da764275b12cb93ece0be762ead4e5
Dmitry <pro100pro10010@gmail.com>
2024-04-01 17:21:51 +0300

    Finish [plagiarism generation] scripts/plagiarism generation

--- scripts/plagiarsim_generation.py
+++ scripts/plagiarsim_generation.py
@@ -1,7 +1,10 @@
 import argparse
 import asyncio
+import contextlib
 import os
+import sys
 import warnings
+from typing import Optional
 
 import g4f
 import nest_asyncio
@@ -16,7 +19,7 @@ from tqdm import tqdm
 nest_asyncio.apply()
 
 ### Constants
-API_ATTEMPTS = 3
+API_ATTEMPTS = 2
 
 DEFAULT_NUM = 2
 RANDOM_STATE = 42
@@ -24,11 +27,11 @@ INITIAL_PERCENT = 0.001
 
 REQUESTS_DELAY = 11
 
-MAX_SYMBOLS = 200
+MAX_SYMBOLS = 150
 
 AVAILABLE_MODELS = [
     "openchat_3.5",
-    "pi",
+    "pi",  # requires google engine
     "mixtral-8x7b",
     # "llama2-70b",
     # "gpt-3.5-turbo",
@@ -37,6 +40,8 @@ AVAILABLE_MODELS = [
     # # "pi", #requires google engine
     # #"airoboros-70b",
 ]
+
+SKIPPED_MODELS = set()
 
 ### Class for Logger instance
 
@@ -61,7 +66,7 @@ LOGGER = Logger()
 
 
 ### Prompts
-def prompt_1(text: str, history: list) -> str:
+def prompt_1(text: str, history: list = []) -> str:
     prefix = "One more time but in different way " if len(history) != 0 else ""
     return f"{prefix}Paraphrase and reformulate the following text as much as you can \
         keeping the initial idea. Use synonyms. Return it as one text line. \
@@ -70,20 +75,22 @@ def prompt_1(text: str, history: list) -> str:
         Let it it be ONLY the paraphrased text in your answer:\n {text}"
 
 
-def prompt_21(text: str) -> str:
-    return f"Write directly in several words the main topic \
+def prompt_21(text: str, history: list = []) -> str:
+    prefix = "One more time but in different way " if len(history) != 0 else ""
+    return f"{prefix}Write directly in several words the main topic \
         of the following text, without details: \n {text}"
 
 
-def prompt_22(text: str) -> str:
-    return f"Write a short sentence (no more than {MAX_SYMBOLS} symbols) \
+def prompt_22(text: str, history: list = []) -> str:
+    prefix = "One more time but in different way " if len(history) != 0 else ""
+    return f"{prefix}Write a short sentence (no more than {MAX_SYMBOLS} symbols) \
         on the following topic: \n {text}"
 
 
 ### Generations
 
 
-async def generate_type1(text: str, model, num: int, loop: tqdm):
+async def generate_type1(text: str, model, num: int, loop: Optional[tqdm] = None):
     data_row = {}
 
     history = []
@@ -94,8 +101,8 @@ async def generate_type1(text: str, model, num: int, loop: tqdm):
             history,
         )
         data_row[f"{model}_{i+1}"] = answer
-        await asyncio.sleep(REQUESTS_DELAY)
-        loop.update(1)
+        if loop is not None:
+            loop.update(1)
 
     return data_row
 
@@ -125,17 +132,89 @@ async def generate_save_type1(dataset: list[str], save_path: str, num: int, desc
             for k, v in d.items():
                 data[k].append(v)
 
-        pd.DataFrame(data).to_csv(construct_absolute_path(save_path, f"{i}.csv"))
+        save_df(pd.DataFrame(data), construct_absolute_path(save_path, f"{i}.csv"))
+
+
+async def generate_type2(idea: str, model, num: int, loop: Optional[tqdm] = None):
+    data_row = {}
+
+    history = []
+    for i in range(num):
+        answer, history = await access_llm(
+            model,
+            prompt_22(idea, history),
+            history,
+        )
+        data_row[f"{model}_{i+1}"] = answer
+        if loop is not None:
+            loop.update(1)
+
+    return data_row
+
+
+async def generate_save_type2(dataset: list[str], save_path: str, num: int, desc: str):
+    data = {}
+
+    data["initial"] = []
+    for model in AVAILABLE_MODELS:
+        for i in range(num):
+            data[f"{model}_{i+1}"] = []
+
+    loop = tqdm(
+        desc=desc,
+        total=len(dataset) * len(AVAILABLE_MODELS) * num,
+        disable=not LOGGER.verbose,
+        leave=False,
+    )
+
+    ideas_data = []
+    ideas_columns = ["initial", "idea", "model"]
+
+    model_idx = 0
+
+    for i, text in enumerate(dataset):
+        really_available_models = list(
+            filter(lambda m: m not in SKIPPED_MODELS, AVAILABLE_MODELS)
+        )
+
+        data["initial"].append(text)
+
+        idea, _ = await access_llm(
+            really_available_models[model_idx],
+            prompt_21(text),
+        )
+        ideas_data.append((text, idea, really_available_models[model_idx]))
+
+        raw_data = await asyncio.gather(
+            *[generate_type2(idea, model, num, loop) for model in AVAILABLE_MODELS]
+        )
+
+        for d in raw_data:
+            for k, v in d.items():
+                data[k].append(v)
+
+        save_df(pd.DataFrame(data), construct_absolute_path(save_path, f"{i}.csv"))
+        save_df(
+            pd.DataFrame(ideas_data, columns=ideas_columns),
+            construct_absolute_path(save_path, f"ideas_{i}.csv"),
+        )
+
+        model_idx = (model_idx + 1) % len(really_available_models)
 
 
 ### Utils
-async def access_llm(model: str, content: str, history: list) -> tuple[str, list]:
+async def access_llm(model: str, content: str, history: list = []) -> tuple[str, list]:
+    if model in SKIPPED_MODELS:
+        return "", history
+
     for i in range(API_ATTEMPTS):
         try:
             response = await g4f.ChatCompletion.create_async(
                 model=model,
                 messages=[*history, {"role": "user", "content": content}],
             )
+
+            await asyncio.sleep(REQUESTS_DELAY)
 
             return response, [
                 *history,
@@ -147,14 +226,104 @@ async def access_llm(model: str, content: str, history: list) -> tuple[str, list
             await asyncio.sleep(REQUESTS_DELAY)
             continue
 
+    SKIPPED_MODELS.add(model)
     return "", history
 
 
+async def fill_gaps_1(file_path: str, num: int, exit_on_fail: bool = True):
+    def get_model(column: str) -> str:
+        return "_".join(list(column.split("_"))[:-1])
+
+    async def inner_loop(i_curr: int, num: int) -> tuple[bool, int, dict]:
+        for i in range(i_curr, len(df)):
+            for j in range(1, len(df.columns)):
+                if df.isnull().iloc[i, j]:
+                    data_row = await generate_type1(
+                        str(df.iloc[i, 0]), get_model(df.columns[j]), num
+                    )
+                    return False, i, data_row
+        return True, i, {}
+
+    LOGGER.log(f"Fixing '{file_path}'...")
+
+    df = pd.read_csv(file_path)
+
+    i_curr = 0
+    no_gap = False
+    while not no_gap:
+        no_gap, i, data_row = await inner_loop(i_curr, num)
+        if not no_gap:
+            lengths = 1
+            for k, v in data_row.items():
+                df.loc[i, k] = v
+                lengths *= len(v)
+            if lengths > 0:
+                i_curr = i + 1
+                LOGGER.log(f"Done {i}!")
+                save_df(df, file_path)
+            elif exit_on_fail:
+                sys.exit(0)
+            else:
+                await asyncio.sleep(REQUESTS_DELAY * 2)
+
+    LOGGER.log(f"Done with '{file_path}'\n")
+
+
+async def fill_gaps_2(
+    file_path: str, ideas_path: str, num: int, exit_on_fail: bool = True
+):
+    def get_model(column: str) -> str:
+        return "_".join(list(column.split("_"))[:-1])
+
+    async def inner_loop(
+        df: pd.DataFrame, ideas_df: pd.DataFrame, i_curr: int, num: int
+    ) -> tuple[bool, int, dict]:
+        for i in range(i_curr, len(df)):
+            for j in range(1, len(df.columns)):
+                if df.isnull().iloc[i, j]:
+                    idea = str(ideas_df.iloc[i, 1])
+
+                    data_row = await generate_type2(idea, get_model(df.columns[j]), num)
+                    return False, i, data_row
+        return True, i, {}
+
+    LOGGER.log(f"Fixing '{file_path}'...")
+
+    df = pd.read_csv(file_path)
+    ideas_df = pd.read_csv(ideas_path)
+
+    i_curr = 0
+    no_gap = False
+    while not no_gap:
+        no_gap, i, data_row = await inner_loop(df, ideas_df, i_curr, num)
+        if not no_gap:
+            lengths = 1
+            for k, v in data_row.items():
+                df.loc[i, k] = v
+                lengths *= len(v)
+            if lengths > 0:
+                i_curr = i + 1
+                LOGGER.log(f"Done {i}!")
+                save_df(df, file_path)
+            elif exit_on_fail:
+                sys.exit(0)
+            else:
+                await asyncio.sleep(REQUESTS_DELAY * 2)
+
+    LOGGER.log(f"Done with '{file_path}'\n")
+
+
+def get_max_index(path: str) -> int:
+    max_index = -1
+    for file in os.listdir(path):
+        with contextlib.suppress(ValueError):
+            max_index = max(max_index, int(os.path.splitext(file)[0]))
+    return max_index
+
+
 def soft_make_dir(path: str):
-    try:
+    with contextlib.suppress(Exception):
         os.mkdir(path)
-    except:
-        pass
 
 
 def construct_absolute_path(*relative_path: str) -> str:
@@ -167,6 +336,15 @@ def construct_absolute_path(*relative_path: str) -> str:
         str: absolute path
     """
     return os.path.abspath(os.path.join(*relative_path))
+
+
+def save_df(df: pd.DataFrame, path: str):
+    df.loc[:, ~df.columns.str.contains("^Unnamed")].to_csv(path, index=False)
+
+
+def prepare_sentences(sentences: list[str]) -> list[str]:
+    permuted = list(np.random.permutation(sentences))
+    return permuted[: int(len(permuted) * INITIAL_PERCENT)]
 
 
 async def generate_plagiarism():
@@ -182,6 +360,13 @@ async def generate_plagiarism():
         choices=[0, 1, 2],
         default="0",
         help="plagiarism types to generate (default: 0 = all)",
+    )
+    parser.add_argument(
+        "-f",
+        "--fixing",
+        default=False,
+        action=argparse.BooleanOptionalAction,
+        help="fix the existing files (default: 0 = all)",
     )
     parser.add_argument(
         "-s",
@@ -227,6 +412,7 @@ async def generate_plagiarism():
     namespace = parser.parse_args()
     (
         plagiarism,
+        fixing,
         save_path,
         data_load_path,
         num,
@@ -234,6 +420,7 @@ async def generate_plagiarism():
         verbose,
     ) = (
         namespace.plagiarism,
+        namespace.fixing,
         namespace.save_path,
         namespace.data_load_path,
         namespace.num,
@@ -258,23 +445,49 @@ async def generate_plagiarism():
     train_sentences = [x[1] for x in train_dataset]
     test_sentences = [x[1] for x in test_dataset]
 
-    np.random.seed(RANDOM_STATE)
-    np.random.shuffle(train_sentences)
-    np.random.shuffle(test_sentences)
+    LOGGER.log(f"Train size = {int(len(train_sentences) * INITIAL_PERCENT)}")
+    LOGGER.log(f"Test size = {int(len(test_sentences) * INITIAL_PERCENT)}\n")
 
-    train_sentences = train_sentences[: int(len(train_sentences) * INITIAL_PERCENT)]
-    test_sentences = test_sentences[: int(len(test_sentences) * INITIAL_PERCENT)]
+    # Fixing
+    if fixing:
+        train1_index = get_max_index(construct_absolute_path(save_path, "train1"))
+        test1_index = get_max_index(construct_absolute_path(save_path, "test1"))
 
-    LOGGER.log(f"{len(train_sentences)=}")
-    LOGGER.log(f"{len(test_sentences)=}")
+        await fill_gaps_1(
+            construct_absolute_path(save_path, "train1", f"{train1_index}.csv"),
+            num,
+        )
+        await fill_gaps_1(
+            construct_absolute_path(save_path, "test1", f"{test1_index}.csv"),
+            num,
+        )
+
+        train2_index = get_max_index(construct_absolute_path(save_path, "train2"))
+        test2_index = get_max_index(construct_absolute_path(save_path, "test2"))
+        await fill_gaps_2(
+            construct_absolute_path(save_path, "train2", f"{train2_index}.csv"),
+            construct_absolute_path(save_path, "train2", f"ideas_{train2_index}.csv"),
+            num,
+        )
+
+        await fill_gaps_2(
+            construct_absolute_path(save_path, "test2", f"{test2_index}.csv"),
+            construct_absolute_path(save_path, "test2", f"ideas_{test2_index}.csv"),
+            num,
+        )
+
+        return
 
     # Generate Type 1 Plagiarism
     if plagiarism <= 1:
+        np.random.seed(RANDOM_STATE)
+        train_sentences1 = prepare_sentences(train_sentences)
+        test_sentences1 = prepare_sentences(test_sentences)
+
         train1_path = construct_absolute_path(save_path, "train1")
         soft_make_dir(train1_path)
         await generate_save_type1(
-            train_sentences,
-            # ["I love chocolate", "Cat eat the mouse", "Elephant is so big in the zoo"],
+            train_sentences1,
             train1_path,
             num,
             "Generating Type 1 for train data",
@@ -284,14 +497,38 @@ async def generate_plagiarism():
         test1_path = construct_absolute_path(save_path, "test1")
         soft_make_dir(test1_path)
         await generate_save_type1(
-            test_sentences,
-            # ["I love chocolate", "Cat eat the mouse", "Elephant is so big in the zoo"],
+            test_sentences1,
             test1_path,
             num,
             "Generating Type 1 for test data",
         )
+        LOGGER.log("Finish generation Type 1 for test data!")
 
-        # LOGGER.log("Finish generation Type 1 for test data!")
+    # Generate Type 2 Plagiarism
+    if plagiarism == 0 or plagiarism == 2:
+        np.random.seed(RANDOM_STATE * 2)
+        train_sentences2 = prepare_sentences(train_sentences)
+        test_sentences2 = prepare_sentences(test_sentences)
+
+        train2_path = construct_absolute_path(save_path, "train2")
+        soft_make_dir(train2_path)
+        await generate_save_type2(
+            train_sentences2,
+            train2_path,
+            num,
+            "Generating Type 2 for train data",
+        )
+        LOGGER.log("Finish generation Type 2 for train data!")
+
+        test2_path = construct_absolute_path(save_path, "test2")
+        soft_make_dir(test2_path)
+        await generate_save_type2(
+            test_sentences2,
+            test2_path,
+            num,
+            "Generating Type 2 for test data",
+        )
+        LOGGER.log("Finish generation Type 2 for test data!")
 
     LOGGER.log("Done!")
 
